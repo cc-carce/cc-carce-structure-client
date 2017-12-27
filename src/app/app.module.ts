e0dab8ab222d7aff37d96ba00d8f30f418e9a8cf
cyolo <33305168+cyolo@users.noreply.github.com>
2017-12-26 21:59:32 -0600

    avance 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    AsideComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
